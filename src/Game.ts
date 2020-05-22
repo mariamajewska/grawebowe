4c25018c72a58a85d1b5c94387843c46dc16ad42
mariamajewska <48626106+mariamajewska@users.noreply.github.com>
2020-05-22 20:21:50 +0200

    fix owner line

--- src/Game.ts
+++ src/Game.ts
@@ -21,10 +21,10 @@ export class Game {
     private readonly pionoweLinie: Line[];
 
     //Gracze 
-   private readonly gracz1: Player = new Player ("red")
-   private readonly gracz2: Player = new Player ("blue")
+    private gracz1: Player = new Player("red")
+    private gracz2: Player = new Player("blue")
 
-   private turaGracza1: boolean = true
+    private turaGracza1: boolean = true
 
     /** Constructor. Nowa gra, ustalam liczbe planszy, rysuje na canvasie */
     constructor(canvas: HTMLCanvasElement, columnNumber: number, rowNumber: number, lineThickness: number, lineLength: number) {
@@ -34,10 +34,10 @@ export class Game {
         this.lineLength = lineLength = 80;
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
-        
-        
+
+
         // array to przechowywania linii
-       
+
         this.poziomeLinie = new Array<Line>(rowNumber);
         this.pionoweLinie = new Array<Line>(columnNumber);
         this.CreateBoxes();
@@ -54,47 +54,54 @@ export class Game {
         console.log("Someone clicked in position x: " + clickPosition.x + " y: " + clickPosition.y);
 
 
-        for (let y = 0; y <this.pionoweLinie.length; y++) 
-        {
-            
-            if (this.pionoweLinie[y].CheckForCollision(clickPosition))  { 
-            if (this.turaGracza1 === true){
-                
-                this.turaGracza1 = false
-                this.context.strokeStyle = this.gracz1.kolorgracza
-                this.pionoweLinie[y].Draw(this.context) 
-            }
-            else {
+        for (let y = 0; y < this.pionoweLinie.length; y++) {
 
-                this.turaGracza1 = true
-                this.context.strokeStyle = this.gracz2.kolorgracza
-                this.pionoweLinie[y].Draw(this.context)
+            if (this.pionoweLinie[y].CheckForCollision(clickPosition)) {
+
+                if (this.pionoweLinie[y].owner === null) {
+
+                    if (this.turaGracza1 === true) {
+
+                        this.turaGracza1 = false
+                        this.context.strokeStyle = this.gracz1.kolorgracza
+                        this.pionoweLinie[y].Draw(this.context)
+                        this.pionoweLinie[y].owner = this.gracz1
+                    }
+                    else {
+
+                        this.turaGracza1 = true
+                        this.context.strokeStyle = this.gracz2.kolorgracza
+                        this.pionoweLinie[y].Draw(this.context)
+                        this.pionoweLinie[y].owner = this.gracz2
+                    }
+                    return //jeżeli grzacz kliknie i zostanie znaleziona kolizja dla pierwszej fukcji to nie będzie już tego wykobywał
+                }
             }
-            return //jeżeli grzacz kliknie i zostanie znaleziona kolizja dla pierwszej fukcji to nie będzie już tego wykobywał
-        }               
-            
         }
 
+        for (let x = 0; x < this.poziomeLinie.length; x++) {
 
-        for (let x = 0; x <this.poziomeLinie.length; x++) 
-        {
-    
-            if (this.poziomeLinie[x].CheckForCollision(clickPosition))  {
+            if (this.poziomeLinie[x].CheckForCollision(clickPosition)) {
 
-               if (this.turaGracza1 === true){
-                
-                this.turaGracza1= false
-                this.context.strokeStyle = this.gracz1.kolorgracza
-                this.poziomeLinie[x].Draw(this.context) 
-            }
-            else {
+                if (this.poziomeLinie[x].owner === null) {
 
-                this.turaGracza1 = true
-                this.context.strokeStyle = this.gracz2.kolorgracza
-                this.poziomeLinie[x].Draw(this.context)
+                    if (this.turaGracza1 === true) {
+
+                        this.turaGracza1 = false
+                        this.context.strokeStyle = this.gracz1.kolorgracza
+                        this.poziomeLinie[x].Draw(this.context)
+                        this.poziomeLinie[x].owner = this.gracz1
+                    }
+                    else {
+
+                        this.turaGracza1 = true
+                        this.context.strokeStyle = this.gracz2.kolorgracza
+                        this.poziomeLinie[x].Draw(this.context)
+                        this.poziomeLinie[x].owner = this.gracz2
+                    }
+                }
             }
-            }
-    }
+        }
     }
 
     //**Pętle które tworzą linie poziome i pionowe przez co tworzą plansze do gry */
@@ -106,7 +113,7 @@ export class Game {
                 const newLine = new Line(this.lineThickness, lineStart, lineEnd);
                 newLine.Draw(this.context);
                 this.poziomeLinie[(x * this.columnNumber) + y] = newLine;
-                
+
             }
         }
 
