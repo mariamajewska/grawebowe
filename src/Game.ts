e69f67ff31d3b384006aa9fc6f1c2cfd0fe52d23
mariamajewska <48626106+mariamajewska@users.noreply.github.com>
2020-06-25 21:09:01 +0200

    check if the squares close

--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,17 @@
 import { Player } from './Player';
 import { Vector2 } from './Vector2';
 import { Line } from './Line';
-
+enum MozliweZamknieciaKwadratow {
+    kwadratgora,
+    kwadratdol,
+    kwadratprawy,
+    kwadratlewy,
+    /**kwadraty poziom zamykają kwadraty przy środkowej lini pionowej  */
+    kwadratypoziom,
+    /**kwadraty pion zamykają kwadraty przy środkowej lini poziomej  */
+    kwadratypion,
+    pustakreskapoziompion
+}
 
 //**Cała Plansza */
 export class Game {
@@ -28,8 +38,8 @@ export class Game {
 
     /** Constructor. Nowa gra, ustalam liczbe planszy, rysuje na canvasie */
     constructor(canvas: HTMLCanvasElement, columnNumber: number, rowNumber: number, lineThickness: number, lineLength: number) {
-        this.columnNumber = columnNumber = 100;
-        this.rowNumber = rowNumber = 100;
+        this.columnNumber = columnNumber = 11;
+        this.rowNumber = rowNumber = 11;
         this.lineThickness = lineThickness = 10;
         this.lineLength = lineLength = 80;
         this.canvas = canvas;
@@ -46,6 +56,68 @@ export class Game {
         // tutaj wywołuje się akcja kiedy ktoś coś naciśnie
         this.canvas.addEventListener("click", this.OnClick.bind(this));
     }
+    /** ...I czy wgl */
+    JakSieKwadratyZamykaja(linia: number, jestpozioma: boolean): MozliweZamknieciaKwadratow {
+        if (jestpozioma) {
+            const clickedLine = this.poziomeLinie[linia];
+            let isSquareAbove = false;
+            let isSquareUnder = false;
+
+            const lineUnder = this.poziomeLinie.length < linia + 1 ? null : this.poziomeLinie[linia + 1];
+            const lineAbove = 0 > linia - 1 ? null : this.poziomeLinie[linia - 1];
+
+            if (lineAbove.owner != null) {
+                const lineLeft = 0 > linia - 1 ? null : this.pionoweLinie[linia - 1];
+                const lineRight = this.pionoweLinie.length < linia + this.rowNumber - 1 ? null : this.pionoweLinie[linia + this.rowNumber - 1];
+                if(lineLeft != null && lineRight != null) isSquareAbove = true;
+            }
+
+            if (lineUnder.owner != null) {
+                const lineLeft = this.pionoweLinie.length < linia ? null : this.pionoweLinie[linia];
+                const lineRight = this.pionoweLinie.length < linia + this.rowNumber ? null : this.pionoweLinie[linia + this.rowNumber];
+                if(lineLeft != null && lineRight != null) isSquareUnder = true;
+            }
+
+            if(isSquareAbove){
+                if(isSquareUnder) return MozliweZamknieciaKwadratow.kwadratypion;
+                else return MozliweZamknieciaKwadratow.kwadratgora;
+            }else if(isSquareUnder){
+                return MozliweZamknieciaKwadratow.kwadratdol;
+            }else{
+                return MozliweZamknieciaKwadratow.pustakreskapoziompion
+            }
+        }
+        else {
+            const clickedLine = this.pionoweLinie[linia];
+            let isSquareRight = false;
+            let isSquareLeft = false;
+
+            const lineLeft = 0 > linia + this.rowNumber ? null : this.pionoweLinie[linia + this.rowNumber];
+            const lineRight = 0 > linia - this.rowNumber ? null : this.pionoweLinie[linia - this.rowNumber];
+
+            if (lineRight.owner != null) {
+                const lineUnder = this.poziomeLinie.length < linia + 1 ? null : this.poziomeLinie[linia + 1];
+                const lineAbove = this.poziomeLinie.length < linia ? null : this.poziomeLinie[linia];
+                if(lineUnder != null && lineAbove != null) isSquareRight = true;
+            }
+
+            if (lineLeft.owner != null) {
+                const lineUnder = 0 > (linia - this.rowNumber) + 1 ? null : this.poziomeLinie[(linia - this.rowNumber) + 1];
+                const lineAbove = 0 > linia - this.rowNumber ? null : this.poziomeLinie[linia - this.rowNumber];
+                if(lineUnder != null && lineAbove != null) isSquareLeft = true;
+            }
+
+            if(isSquareRight){
+                if(isSquareLeft) return MozliweZamknieciaKwadratow.kwadratypoziom;
+                else return MozliweZamknieciaKwadratow.kwadratprawy;
+            }else if(isSquareLeft){
+                return MozliweZamknieciaKwadratow.kwadratlewy;
+            }else{
+                return MozliweZamknieciaKwadratow.pustakreskapoziompion
+            }
+        }
+    }
+
 
     //**Funkcja która się wywołuje gdy gracz nacisnie gdzieś na planszy */
     OnClick(event: MouseEvent): void {
@@ -57,7 +129,7 @@ export class Game {
         for (let y = 0; y < this.pionoweLinie.length; y++) {
 
             if (this.pionoweLinie[y].CheckForCollision(clickPosition)) {
-
+                console.log("Clicked vertical line: " + y);
                 if (this.pionoweLinie[y].owner === null) {
 
                     if (this.turaGracza1 === true) {
@@ -82,7 +154,7 @@ export class Game {
         for (let x = 0; x < this.poziomeLinie.length; x++) {
 
             if (this.poziomeLinie[x].CheckForCollision(clickPosition)) {
-
+                console.log("Clicked horizontal line: " + x);
                 if (this.poziomeLinie[x].owner === null) {
 
                     if (this.turaGracza1 === true) {
